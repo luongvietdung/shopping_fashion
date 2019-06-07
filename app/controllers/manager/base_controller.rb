e4d7e5b29200f7b4034b2370a8a2f9502d7e1f4a
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-06-07 16:00:43 +0700

    fix login

--- app/controllers/manager/base_controller.rb
+++ app/controllers/manager/base_controller.rb
@@ -2,6 +2,15 @@
 
 module Manager
   class BaseController < ApplicationController
+    before_action :check_login
     layout "admin"
+
+    private
+
+    def check_login
+      return if current_admin.present?
+
+      redirect_to new_admin_session_path
+    end
   end
 end
--- app/controllers/manager/sessions_controller.rb
+++ app/controllers/manager/sessions_controller.rb
@@ -3,5 +3,12 @@
 module Manager
   class SessionsController < Devise::SessionsController
     layout "admin"
+    def after_sign_in_path_for(resource)
+      stored_location_for(resource) || manager_root_url
+    end
+
+    def after_sign_out_path_for(resource)
+      stored_location_for(resource) || manager_root_url
+    end
   end
 end
