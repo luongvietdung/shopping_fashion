09bd9df363eabadf4130e9edd96c0f91ee340575
Linh <tomroket@gmail.com>
2019-06-07 10:37:15 +0800

    db:migrate

--- db/migrate/20190607014221_create_products.rb
+++ db/migrate/20190607014221_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.string :name
-      t.double :price
+      t.decimal :price
       t.integer :quantity
       t.text :description
       t.references :admin, foreign_key: true
