6e16ecb188c44adb39ac23b222845d4acb6a26df
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-06-19 13:50:50 +0700

    create-model-orders

--- db/migrate/20190619050011_create_orders.rb
+++ db/migrate/20190619050011_create_orders.rb
@@ -1,11 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
-      t.string, :address
-      t.string, :phone_number
-      t.string, :full_name
+      t.string :address
+      t.string :phone_number
+      t.string :full_name
       t.integer :status
-
+      t.reference :user,foreign_key: true
+      
       t.timestamps
     end
   end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:phone_number) }
+  it { should validate_presence_of(:full_name) }
+
+  it { should validate_length_of(:address).is_at_most(150) }
+  it { should validate_length_of(:phone_number).is_at_least(10) }
+  it { should validate_length_of(:full_name).is_at_most(50) }
 end
